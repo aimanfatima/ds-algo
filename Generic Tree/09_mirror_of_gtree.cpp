27ed44ba0d7d42c43c61dcc72f866e694b0224a3
Aiman Fatima <aimanfatimadl@gmail.com>
2020-01-06 23:21:43 +0530

    Others gtree operations

---
+++ Generic Tree/09_mirror_of_gtree.cpp
@@ -0,0 +1,83 @@
+//  10, 20, 50, -1, 60, -1, -1, 30, 70, -1, 80, 110, -1, 120, -1, -1, 90, -1, -1, 40, 100, -1, -1, -1
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+    public:
+    int data;
+    vector<Node*> children;
+
+    Node(int data)
+    {
+        this->data = data;
+    }
+};
+
+Node* construct(vector<int>& dlist)
+{
+    stack<Node*> st;
+    Node* root = NULL;
+
+    for(int i=0; i<dlist.size(); i++)
+    {
+        if(dlist[i]!=-1)
+        {
+            Node* node = new Node(dlist[i]);
+            if(st.empty())
+            {
+                root = node;
+            }
+            else
+            {
+                st.top()->children.push_back(node);
+            }
+
+            st.push(node);
+        }
+        else
+        {
+            st.pop();
+        }
+    }
+    return root;
+}
+
+void display(Node* root)
+{
+    cout<<root->data<<"     .   ";
+    for(int i=0; i<root->children.size(); i++)
+        cout<<root->children[i]->data<<"    ";
+    cout<<endl;
+
+    for(int i=0; i<root->children.size(); i++)
+        display(root->children[i]);
+}
+
+void mirror(Node* root)
+{
+    for(int i=0; i<root->children.size(); i++)
+    {
+        mirror(root->children[i]);
+    }
+
+    int i=0, j=root->children.size()-1;
+    while(i<j)
+    {
+        Node* temp = root->children[i];
+        root->children[i] = root->children[j];
+        root->children[j] = temp;
+        i++;j--;
+    }
+}
+
+int main()
+{
+    vector<int> dlist = { 10, 20, 50, -1, 60, -1, -1, 30, 70, -1, 80, 110, -1, 120, -1, -1, 90, -1, -1, 40, 100, -1, -1, -1};
+    Node* root = construct(dlist);
+    display(root);
+    cout<<endl;
+    mirror(root);
+    display(root);
+}
