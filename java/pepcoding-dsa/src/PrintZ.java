5f729cedf2a7707945858d29acf1043d9e7288db
Aiman Fatima <aimanfatimadl@gmail.com>
2023-07-15 18:57:36 +0530

    Print Z

---
+++ java/pepcoding-dsa/src/PrintZ.java
@@ -0,0 +1,13 @@
+public class PrintZ {
+    /**
+     * PRINT Z
+     *   *****
+     *      *
+     *     *
+     *    *
+     *   *****
+     */
+    public static void main (String[] args) {
+        System.out.println("*****\n   *\n  *\n *\n*****");
+    }
+}
