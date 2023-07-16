e2e264b1cfb1af962a3fbe84f70361d5005a9239
Aiman Fatima <aimanfatimadl@gmail.com>
2023-07-16 11:47:16 +0530

    Grading system

---
+++ java/pepcoding-dsa/src/GradingSystem.java
@@ -0,0 +1,17 @@
+public class GradingSystem {
+    public static void main (String[] args) {
+        int marks = 96;
+
+        if (marks > 90) {
+            System.out.println("Excellent");
+        } else if (marks > 80 && marks <=90) {
+            System.out.println("Good");
+        } else if (marks > 70 && marks <= 80) {
+            System.out.println("fair");
+        } else if (marks > 60 && marks <= 70) {
+            System.out.println("Meets Expectations");
+        } else {
+            System.out.println("Below par");
+        }
+    }
+}
