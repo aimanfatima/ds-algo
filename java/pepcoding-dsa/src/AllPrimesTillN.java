776673cb0679b43e29ebffd765bb9d7954eb2689
Aiman Fatima <aimanfatimadl@gmail.com>
2023-07-16 14:05:11 +0530

    All Primes in a given range

---
+++ java/pepcoding-dsa/src/AllPrimesTillN.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class AllPrimesTillN {
+    public static void main (String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int low = scanner.nextInt();
+        int high = scanner.nextInt();
+
+        for (int i = low; i <= high ; i++) {
+            if (isPrime(i)) {
+                System.out.println(i);
+            }
+        }
+    }
+
+    private static boolean isPrime(int num) {
+        for (int i=2; i*i <= num; i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
