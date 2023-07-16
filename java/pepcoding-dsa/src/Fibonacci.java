22f99faca39858d811e20d26a893cea8ccc5343a
Aiman Fatima <aimanfatimadl@gmail.com>
2023-07-16 18:07:21 +0530

    Fibonacci till n

---
+++ java/pepcoding-dsa/src/Fibonacci.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Fibonacci {
+    public static void main (String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+
+        int first = 0;
+        int second = 1;
+        for (int i=1; i<=n ; i++) {
+            if (i==1) {
+                System.out.print(first + " ");
+            } else if (i==2) {
+                System.out.print(second + " ");
+            } else {
+                int next = first + second;
+                first = second;
+                second = next;
+                System.out.print(next + " ");
+            }
+        }
+
+    }
+}
