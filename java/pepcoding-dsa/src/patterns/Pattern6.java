1cda5981afe75433b58166c490e8570e4409215b
Aiman Fatima <aimanfatimadl@gmail.com>
2023-07-19 21:59:48 +0530

    Pattern 6

---
+++ java/pepcoding-dsa/src/patterns/Pattern6.java
@@ -0,0 +1,46 @@
+package patterns;
+
+/**
+
+ *	*	*	*	*	*	 	*	*	*	*	*	*
+ *	*	*	*	*	 	 	 	*	*	*	*	*
+ *	*	*	*	 	 	 	 	 	*	*	*	*
+ *	*	*	 	 	 	 	 	 	 	*	*	*
+ *	*	 	 	 	 	 	 	 	 	 	*	*
+ *	 	 	 	 	 	 	 	 	 	 	 	*
+ *	*	 	 	 	 	 	 	 	 	 	*	*
+ *	*	*	 	 	 	 	 	 	 	*	*	*
+ *	*	*	*	 	 	 	 	 	*	*	*	*
+ *	*	*	*	*	 	 	 	*	*	*	*	*
+ *	*	*	*	*	*	 	*	*	*	*	*	*
+
+ */
+
+public class Pattern6 {
+    public static void main(String[] args) {
+        int n = 11;
+
+        int st = n/2 + 1;
+        int sp = 1;
+
+        for (int i=1; i<=n; i++) {
+            for (int j=1; j<=st; j++) {
+                System.out.print("*\t");
+            }
+            for (int j=1; j<=sp; j++) {
+                System.out.print(" \t");
+            }
+            for (int j=1; j<=st; j++) {
+                System.out.print("*\t");
+            }
+            if (i <= n/2) {
+                st --;
+                sp += 2;
+            } else {
+                st++;
+                sp -= 2;
+            }
+            System.out.println();
+        }
+    }
+}
