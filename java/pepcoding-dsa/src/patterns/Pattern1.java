836de1ce3e548c8422c4341187dbb213cef323af
Aiman Fatima <aimanfatimadl@gmail.com>
2023-07-19 19:51:16 +0530

    Pattern 1

---
+++ java/pepcoding-dsa/src/patterns/Pattern1.java
@@ -0,0 +1,17 @@
+package patterns;
+
+import java.util.Scanner;
+
+public class Pattern1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+
+        for (int i=0; i<n; i++) {
+            for (int j=0; j<=i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
