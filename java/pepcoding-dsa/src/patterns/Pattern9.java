ebff27552eedef46633712301f80f23a542ad540
Aiman Fatima <aimanfatimadl@gmail.com>
2023-07-19 22:25:05 +0530

    Pattern 9

---
+++ java/pepcoding-dsa/src/patterns/Pattern9.java
@@ -0,0 +1,29 @@
+package patterns;
+
+/**
+CROSS
+
+ *	 	 	 	*
+    *	 	*
+        *
+     *	 	*
+ *	 	 	 	*
+
+ */
+
+public class Pattern9 {
+    public static void main(String[] args) {
+        int n = 5;
+
+        for (int i=1; i<=n; i++) {
+            for (int j=1; j<=n; j++) {
+                if (i==j || i+j==n+1) {
+                    System.out.print("*\t");
+                } else {
+                    System.out.print(" \t");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
