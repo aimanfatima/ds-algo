fcecb449a28e31b1cac77976850efce3828aeca6
Aiman Fatima <aimanfatimadl@gmail.com>
2023-08-12 21:12:48 +0530

    Pattern 18

---
+++ java/pepcoding-dsa/src/patterns/Pattern18.java
@@ -0,0 +1,45 @@
+package patterns;
+
+/*
+
+HOUR GLASS
+
+*   *   *   *   *   *   *
+    *               *
+        *       *
+            *
+        *   *   *
+    *   *   *   *   *
+*   *   *   *   *   *   *
+
+
+ */
+
+public class Pattern18 {
+    public static void main(String[] args) {
+        int n = 7;
+        int st = 7;
+        int sp = 0;
+
+        for (int i=1; i<=n; i++) {
+            for (int j=1; j<=sp; j++) {
+                System.out.print(" \t");
+            }
+            for (int j=1; j<=st; j++) {
+                if (i>1 && i<=n/2 && j!=1 && j!=st) {
+                    System.out.print(" \t");
+                } else {
+                    System.out.print("*\t");
+                }
+            }
+            if (i <=n/2) {
+                st -= 2;
+                sp++;
+            } else {
+                st += 2;
+                sp--;
+            }
+            System.out.println();
+        }
+    }
+}
