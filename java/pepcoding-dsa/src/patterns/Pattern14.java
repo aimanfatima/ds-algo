65296385b6dc3ea7f48c7d4ed420b9febcb3a0a4
Aiman Fatima <aimanfatimadl@gmail.com>
2023-08-12 17:37:33 +0530

    Pattern 14

---
+++ java/pepcoding-dsa/src/patterns/Pattern14.java
@@ -0,0 +1,11 @@
+package patterns;
+
+public class Pattern14 {
+    // PRINT table
+    public static void main(String[] args) {
+        int n = 2;
+        for (int i=1; i<=10; i++) {
+            System.out.println(n + " * " + i + " = " + n*i);
+        }
+    }
+}
