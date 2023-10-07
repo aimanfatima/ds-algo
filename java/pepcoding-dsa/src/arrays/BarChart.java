234ab8fe751282ab24fcc72adfc3f987c3893f6c
Aiman Fatima <aimanfatimadl@gmail.com>
2023-10-07 13:26:35 -0700

    Bar chart refactor

--- java/pepcoding-dsa/src/arrays/BarChart.java
+++ java/pepcoding-dsa/src/arrays/BarChart.java
@@ -22,16 +22,13 @@ public class BarChart {
                 max = arr[i];
             }
         }
-        for (int i=0; i<arr.length; i++) {
-            arr[i] = max - arr[i];
-        }
-        for (int i=0; i<max; i++) {
+
+        for (int i=max; i>=1; i--) {
             for (int j=0; j<arr.length; j++) {
-                if (arr[j] > 0) {
-                    System.out.print(" \t");
-                    arr[j]--;
-                } else {
+                if (arr[j] >= i) {
                     System.out.print("*\t");
+                } else {
+                    System.out.print(" \t");
                 }
             }
             System.out.println();
