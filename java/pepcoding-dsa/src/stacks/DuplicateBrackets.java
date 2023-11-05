4d2877e913978dad8dc6bc770d91ee1fbeebd728
Aiman Fatima <aimanfatimadl@gmail.com>
2023-11-05 03:49:34 -0800

    Add comments

--- java/pepcoding-dsa/src/stacks/DuplicateBrackets.java
+++ java/pepcoding-dsa/src/stacks/DuplicateBrackets.java
@@ -3,6 +3,10 @@ package stacks;
 import java.util.Scanner;
 import java.util.Stack;
 
+
+// check for duplicate or needless brackets
+// Solution - keep popping from the stack till you get a '(',
+// if you directly pop a ( with no additional characters or operators then it has unnecessary brackets
 public class DuplicateBrackets {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
