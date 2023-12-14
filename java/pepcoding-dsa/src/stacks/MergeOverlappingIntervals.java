27014d3f09c86c8832232b8ca5b7f01c7f0e7277
Aiman Fatima <aimanfatimadl@gmail.com>
2023-12-13 17:38:38 -0800

    Mark important

--- java/pepcoding-dsa/src/stacks/MergeOverlappingIntervals.java
+++ java/pepcoding-dsa/src/stacks/MergeOverlappingIntervals.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.Scanner;
 import java.util.Stack;
 
+// Important Question - Stack
 public class MergeOverlappingIntervals {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
