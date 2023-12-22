713f17bd454a05c0e711c9ed63c8126195abaf68
Aiman Fatima <aimanfatimadl@gmail.com>
2023-12-22 03:59:01 -0800

    refactor

--- java/pepcoding-dsa/src/recursionandbacktracking/AL_AllIndicesInAnArray.java
+++ java/pepcoding-dsa/src/recursionandbacktracking/AL_AllIndicesInAnArray.java
@@ -4,8 +4,8 @@ public class AL_AllIndicesInAnArray {
     public static void main(String[] args) {
         int[] arr = {2, 2, 1, 3, 8, 5, 0, 2};
         int[] allIndices = allIndicesInAnArray(arr, 2, 0, 0);
-        for (int i=0; i<allIndices.length; i++) {
-            System.out.print(allIndices[i] + " ");
+        for (int allIndex : allIndices) {
+            System.out.print(allIndex + " ");
         }
     }
 
