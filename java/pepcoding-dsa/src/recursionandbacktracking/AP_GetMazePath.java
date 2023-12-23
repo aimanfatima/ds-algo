9d4f81feb7441ca836d37fc9e2a5a6bce2c2bbaf
Aiman Fatima <aimanfatimadl@gmail.com>
2023-12-22 23:42:30 -0800

    refactor maze path

--- java/pepcoding-dsa/src/recursionandbacktracking/AP_GetMazePath.java
+++ java/pepcoding-dsa/src/recursionandbacktracking/AP_GetMazePath.java
@@ -4,8 +4,8 @@ import java.util.ArrayList;
 
 public class AP_GetMazePath {
     public static void main(String[] args) {
-        int rows = 3;
-        int cols = 3;
+        int rows = 2;
+        int cols = 2;
 
         ArrayList<String> getAllMazePaths = getAllMazePaths(0, 0, rows-1, cols-1);
         System.out.println(getAllMazePaths);
@@ -13,24 +13,27 @@ public class AP_GetMazePath {
 
     private static ArrayList<String> getAllMazePaths(int currentRow, int currentCol, int totalRows, int totalCols) {
 
-        if (currentRow >= totalRows || currentCol >= totalCols) {
+        if (currentRow == totalRows && currentCol == totalCols) {
             ArrayList<String> baseResult = new ArrayList<>();
-            if (currentRow == totalRows || currentCol == totalCols) {
-                baseResult.add("");
-            }
+            baseResult.add("");
             return baseResult;
         }
 
-        ArrayList<String> allPathsFromNextCol = getAllMazePaths(currentRow, currentCol + 1, totalRows, totalCols);
-        ArrayList<String> allPathsFromNextRow = getAllMazePaths(currentRow + 1, currentCol, totalRows, totalCols);
         ArrayList<String> allPossiblePaths = new ArrayList<>();
 
-        for (String path : allPathsFromNextCol) {
-            allPossiblePaths.add("h" + path);
+        if (currentCol < totalCols) {
+            ArrayList<String> allPathsFromNextCol = getAllMazePaths(currentRow, currentCol + 1, totalRows, totalCols);
+            for (String path : allPathsFromNextCol) {
+                allPossiblePaths.add("h" + path);
+            }
         }
 
-        for (String path : allPathsFromNextRow) {
-            allPossiblePaths.add("v" + path);
+        if (currentRow < totalRows) {
+            ArrayList<String> allPathsFromNextRow = getAllMazePaths(currentRow + 1, currentCol, totalRows, totalCols);
+
+            for (String path : allPathsFromNextRow) {
+                allPossiblePaths.add("v" + path);
+            }
         }
 
         return allPossiblePaths;
