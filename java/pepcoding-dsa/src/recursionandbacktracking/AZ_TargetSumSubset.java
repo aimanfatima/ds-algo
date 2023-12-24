eb5a1789ab7c5e80e92479cfcec5ffa18ab1b1fc
Aiman Fatima <aimanfatimadl@gmail.com>
2023-12-23 20:30:41 -0800

    Print target sum

---
+++ java/pepcoding-dsa/src/recursionandbacktracking/AZ_TargetSumSubset.java
@@ -0,0 +1,33 @@
+package recursionandbacktracking;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class AZ_TargetSumSubset {
+    public static void main(String[] args) {
+        ArrayList<Integer> arr = new ArrayList<>();
+        arr.add(10);
+        arr.add(20);
+        arr.add(30);
+        arr.add(40);
+        arr.add(50);
+        printTargetSumSubsets(arr, 100,0, "");
+        System.out.println("No of recursive calls =" + count);
+    }
+
+    static int count = 0;
+
+    private static void printTargetSumSubsets(ArrayList<Integer> arr, int target, int index, String answerSoFar) {
+        if (target == 0) {
+            System.out.println(answerSoFar);
+            return;
+        }
+        if (index == arr.size()) {
+            return;
+        }
+        int num = arr.get(index);
+        count += 2;
+        printTargetSumSubsets(arr, target - num, index + 1,answerSoFar + " " + num);
+        printTargetSumSubsets(arr, target, index+1,  answerSoFar);
+    }
+}
