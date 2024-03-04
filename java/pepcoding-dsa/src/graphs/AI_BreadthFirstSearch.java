e2e00d9742d5ce1f01645d976c01c96a6d875ef8
Aiman Fatima <aimanfatimadl@gmail.com>
2024-03-03 21:09:34 -0800

    Graph: BFS

---
+++ java/pepcoding-dsa/src/graphs/AI_BreadthFirstSearch.java
@@ -0,0 +1,47 @@
+package graphs;
+
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+public class AI_BreadthFirstSearch {
+    public static class Pair {
+        int vertex;
+        String path;
+
+        Pair(int vertex, String path) {
+            this.vertex = vertex;
+            this.path = path;
+        }
+    }
+    public static void main(String[] args) {
+        int n = 7;
+        int[][] graph = {
+                {1, 3},
+                {0, 2},
+                {1, 3},
+                {0, 2, 4},
+                {3, 5, 6},
+                {4, 6},
+                {4, 5}
+        };
+
+        int source = 2;
+        Pair pair = new Pair(source, String.valueOf(source));
+        Queue<Pair> queue = new ArrayDeque<>();
+        queue.offer(pair);
+
+        boolean[] visited = new boolean[n];
+
+        while (!queue.isEmpty()) {
+            Pair element = queue.poll();
+            visited[element.vertex] = true;
+            System.out.println(element.path);
+            for (int nbr: graph[element.vertex]) {
+                if (!visited[nbr]) {
+                    queue.offer(new Pair(nbr, element.path+nbr));
+                }
+            }
+        }
+
+    }
+}
