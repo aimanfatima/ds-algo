f4625824a3594bfec1110421ce8f812dcfb6577c
Aiman Fatima <aimanfatimadl@gmail.com>
2024-03-05 20:50:49 -0800

    Graphs: is cyclic refactor

--- java/pepcoding-dsa/src/graphs/AJ_IsGraphCyclic.java
+++ java/pepcoding-dsa/src/graphs/AJ_IsGraphCyclic.java
@@ -28,19 +28,36 @@ public class AJ_IsGraphCyclic {
 //        };
 
         // a graph WITH cycle - should return true
+//        int[][] graph = {
+//                {1, 3},
+//                {0, 2},
+//                {1, 3},
+//                {0, 2, 4},
+//                {3, 5, 6},
+//                {4, 6},
+//                {4, 5}
+//        };
+
+        // Note we need to call bfs for every vertex, because there could be a cycle in one of the connected components
+        // if we only start with 0, it might not report a cycle in other cycles
+        // a graph WITH one cycle and 2 connected components
         int[][] graph = {
-                {1, 3},
-                {0, 2},
-                {1, 3},
-                {0, 2, 4},
-                {3, 5, 6},
+                {1},
+                {0},
+                {3},
+                {2},
+                {5, 6},
                 {4, 6},
                 {4, 5}
         };
 
         boolean[] visited = new boolean[n];
 
-        System.out.println(isGraphCyclic(graph, visited, 0));
+        for (int source = 0; source < n; source++) {
+            if (!visited[source]) {
+                System.out.println(isGraphCyclic(graph, visited, source));
+            }
+        }
     }
 
     private static boolean isGraphCyclic(int[][] graph, boolean[] visited, int source) {
