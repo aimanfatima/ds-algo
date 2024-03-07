ded2c2718b934cca4a37a89132bc09711ed5ae65
Aiman Fatima <aimanfatimadl@gmail.com>
2024-03-06 22:07:49 -0800

    Graphs: Spread infection question

---
+++ java/pepcoding-dsa/src/graphs/AM_SpreadInfection.java
@@ -0,0 +1,13 @@
+package graphs;
+
+public class AM_SpreadInfection {
+    public static void main(String[] args) {
+        // Given a graph representing people and their connectivity
+        // You are also given a source perosn who got infected initially at t=0,
+        // and you're given a time t
+        // You are required to calculate how many people will get infected in time t;
+        // if the infection spreads to the neighbours in 1 unit of time
+
+
+    }
+}
